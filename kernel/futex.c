621f23b7d12c116ef446c5430b1f4178ef3ca65d
googyanas <googy@localhost.localdomain>
2014-08-10 19:41:42 +0100

    3.4.59

--- kernel/futex.c
+++ kernel/futex.c
@@ -61,7 +61,7 @@
 #include <linux/nsproxy.h>
 #include <linux/ptrace.h>
 #include <linux/freezer.h>
-
+#include <linux/hugetlb.h>
 #include <asm/futex.h>
 
 #include "rtmutex_common.h"
@@ -364,7 +364,7 @@ again:
 	} else {
 		key->both.offset |= FUT_OFF_INODE; /* inode-based key */
 		key->shared.inode = page_head->mapping->host;
-		key->shared.pgoff = page_head->index;
+		key->shared.pgoff = basepage_index(page);
 	}
 
 	get_futex_key_refs(key);
