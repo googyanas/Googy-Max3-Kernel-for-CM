ae20dec5f633302241fbf55f3b0d2a03a437374f
googyanas <googyanas@gmail.com>
2014-08-11 13:10:31 +0100

    buildenv

--- build_kernel_cm.sh
+++ build_kernel_cm.sh
@@ -3,9 +3,9 @@ export KERNELDIR=`readlink -f .`
 export RAMFS_SOURCE="/home/googy/Anas/Googy-Max3-Kernel/Kernel_cm/ramfs_cm"
 export PARENT_DIR=`readlink -f ..`
 export USE_SEC_FIPS_MODE=true
-export CROSS_COMPILE=/usr/bin/arm-linux-gnu-
+# export CROSS_COMPILE=/usr/bin/arm-linux-gnu-
 # export CROSS_COMPILE=/home/googy/Anas/linaro_a15_4.7.4-2014.04/bin/arm-gnueabi-
-# export CROSS_COMPILE=/home/googy/Anas/linaro_a15_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=/home/googy/Anas/linaro_a15_4.9.1-2014.07/bin/arm-cortex_a15-linux-gnueabihf-
 
 # if [ "${1}" != "" ];then
 #  export KERNELDIR=`readlink -f ${1}`
@@ -31,7 +31,7 @@ make 0googymax3_cm_defconfig VARIANT_DEFCONFIG=jf_eur_defconfig SELINUX_DEFCONFI
 . $KERNELDIR/.config
 
 cd $KERNELDIR/
-make -j2 || exit 1
+make -j3 || exit 1
 
 #remove previous ramfs files
 rm -rf $RAMFS_TMP
